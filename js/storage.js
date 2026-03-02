2fe29b542310425c2146761a0bef0ba5f0d321cf
kaemmermil <nico.kaemmer@me.com>
2026-03-02 19:28:44 +0100

    Implement Storage object with get, set, and generateNumber methods

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,19 @@
+const Storage = {
 
+    get(key){
+        return JSON.parse(localStorage.getItem(key) || "[]");
+    },
+
+    set(key,data){
+        localStorage.setItem(key,JSON.stringify(data));
+    },
+
+    generateNumber(prefix,key){
+        let counter = localStorage.getItem(key) || 1;
+        let year = new Date().getFullYear();
+        let number = year + "-" + prefix + "-" + String(counter).padStart(4,"0");
+        localStorage.setItem(key, ++counter);
+        return number;
+    }
+
+};
